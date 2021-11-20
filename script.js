319c831d5ee02b1e81cfd653b2590e875ed4e0e5
K.S <spiritual-programmer>
2021-11-19 20:49:36 -0800

    added runtime input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+const prompt = require('prompt-sync')({sigint:true});
 
 const authors = [
   {
@@ -27,21 +28,38 @@ class Book {
     this.author = author;
   }
 };
+//Comment reserved for when testing
+//const input = "Carrie,06/13/1982,Steven King,762\nThe Stand,01/01/1984,Steven King,1200\nThe Hunger Games,11/12/2013,Suzanne Collins,600\nThe Jungle Book,03/21/1898,Rudyard Kipling,483\nKim,02/03/1910,Rudyard Kipling,526\nI Robot,08/11/1945,Isaac Asimov,474\nFirestarter,04/01/1982,Steven King,597";
 
-const input = "Carrie,06/13/1982,Steven King,762\nThe Stand,01/01/1984,Steven King,1200\nThe Hunger Games,11/12/2013,Suzanne Collins,600\nThe Jungle Book,03/21/1898,Rudyard Kipling,483\nKim,02/03/1910,Rudyard Kipling,526\nI Robot,08/11/1945,Isaac Asimov,474\nFirestarter,04/01/1982,Steven King,597";
+//Get input from user
+let splitInputByBook = []
+console.log(`Enter books formatted as Title,Date,Author,Length with date formatted as mm/dd/yyyy
+Enter books one at a time and push enter once finished
+Example: Carrie,06/13/1982,Steven King,762
+=>`)
+while (true) {
+  let input = prompt();
+  splitInputByBook.push(input);
+  if (input == "") {
+    splitInputByBook.pop()
+    break
+  }
+}
 
 function parseInput(input) {
   if (input == null || input.length === 0) throw "Invalid Input"
   else {
-    const splitInputByBook = input => input.split(/\n/);
-    const booksArray = splitInputByBook(input).filter(book => book.split(/,/).length === 4)
+//     const splitInputByBook = input => input.split(/\n/);
+//     const booksArray = splitInputByBook(input).filter(book => book.split(/,/).length === 4)
+    const booksArray = splitInputByBook.filter(book => book.split(/,/).length === 4)
     if (booksArray.length < 1) throw "Invalid Input"
     const booksArrayByDetails = booksArray.map(book => book.split(","));
     const books = booksArrayByDetails.map(book => new Book(book[0],book[1],book[2],book[3]));
     return books
   }  
 }
-const books = parseInput(input);
+const books = parseInput(splitInputByBook);
+//const books = parseInput(input);
 
 function findAuthorWithMostBooks(books) {
   let dict = {};
